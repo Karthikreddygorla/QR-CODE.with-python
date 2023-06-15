5a3088a449bf9e28464f8d5718223668c4256da3
Karthik reddy gorla <116546275+Karthikreddygorla@users.noreply.github.com>
2023-06-15 16:54:35 +0530

    qr.py

--- qr.py
+++ qr.py
@@ -2,4 +2,4 @@ import qrcode
 features=qrcode.QRCode(version=2,box_size=6,border=9)
 features.make(fit=True)
 img=qrcode.make("https://instagram.com/karthikreddygorla?igshid=YmMyMTA2M2Y=")
-img.save("img.png")
+img.save("qr.png") 
